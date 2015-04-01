f6b0b6100fa77b01fdfac47d5531ea0863c62107
tourenathan <teyou21@gmail.com>
2015-04-01 17:51:56 +0100

    Change the view of the first element of the listview to use list_item_forecast_today.xml

--- src/com/icetech/sunshineapp/ForecastAdapter.java
+++ src/com/icetech/sunshineapp/ForecastAdapter.java
@@ -18,6 +18,11 @@ import android.widget.TextView;
 public class ForecastAdapter extends CursorAdapter {
 
 
+	private static final int VIEW_TYPE_TODAY = 0;
+	private static final int VIEW_TYPE_FUTURE_DAY = 1;
+	private static final int VIEW_TYPE_COUNT = 2;
+
+
 	// Flag to determine if we want to use a separate view for "today".
 	private boolean mUseTodayLayout = true;
 
@@ -27,9 +32,35 @@ public class ForecastAdapter extends CursorAdapter {
 	}
 
 	@Override
-	public View newView(Context context, Cursor cursor, ViewGroup parent) {
+	public int getItemViewType(int position) {
 
-		return LayoutInflater.from(context).inflate(R.layout.list_item_forcast, parent, false);
+		return (position == 0)? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+	}
+
+	@Override
+	public int getViewTypeCount() {
+
+		return VIEW_TYPE_COUNT;
+	}
+
+	@Override
+	public View newView(Context context, Cursor cursor, ViewGroup parent) {
+		//Get the view type
+		int viewType = cursor.getPosition();
+		int layoutId = -1;
+		
+		switch (getItemViewType(viewType)) {
+		case VIEW_TYPE_TODAY:
+			layoutId = R.layout.list_item_forecast_today;
+			break;
+			
+		case VIEW_TYPE_FUTURE_DAY:
+			layoutId = R.layout.list_item_forcast;
+
+			break;
+		}
+
+		return LayoutInflater.from(context).inflate(layoutId, parent, false);
 
 	}
 
@@ -63,7 +94,7 @@ public class ForecastAdapter extends CursorAdapter {
 		double high = cursor.getDouble(ForecastFragment.COL_WEATHER_MAX_TEMP);
 		TextView highTempView = (TextView) view.findViewById(R.id.list_item_high_textview);
 		highTempView.setText(Utility.formatTemperature(high, isMetric)+"\u00B0");
-		
+
 		// Read low temperature from cursor
 		double low = cursor.getDouble(ForecastFragment.COL_WEATHER_MIN_TEMP);
 		TextView lowTempView = (TextView) view.findViewById(R.id.list_item_low_textview);
