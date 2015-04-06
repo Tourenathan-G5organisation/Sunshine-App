33b3dbcf5a0bc659ab3c68b4f224e202bc48ad2e
tourenathan <teyou21@gmail.com>
2015-04-06 17:53:55 +0100

    Added the color resource and the touch_selector files to the project, also used the touch_selector as background in the list_item_forcast and list_item_forcast_today layouts

--- gen/com/icetech/sunshineapp/R.java
+++ gen/com/icetech/sunshineapp/R.java
@@ -9,20 +9,24 @@ package com.icetech.sunshineapp;
 
 public final class R {
     public static final class array {
-        public static final int pref_sync_frequency_titles=0x7f080002;
-        public static final int pref_sync_frequency_values=0x7f080003;
-        public static final int pref_temp_list_titles=0x7f080000;
-        public static final int pref_temp_list_values=0x7f080001;
+        public static final int pref_sync_frequency_titles=0x7f090002;
+        public static final int pref_sync_frequency_values=0x7f090003;
+        public static final int pref_temp_list_titles=0x7f090000;
+        public static final int pref_temp_list_values=0x7f090001;
     }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int grey=0x7f060000;
+        public static final int sunshine_light_blue=0x7f060001;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
          */
-        public static final int activity_horizontal_margin=0x7f060000;
-        public static final int activity_vertical_margin=0x7f060001;
-        public static final int list_minHeight=0x7f060003;
-        public static final int listfragment_margin_top=0x7f060002;
+        public static final int activity_horizontal_margin=0x7f070000;
+        public static final int activity_vertical_margin=0x7f070001;
+        public static final int list_minHeight=0x7f070003;
+        public static final int listfragment_margin_top=0x7f070002;
     }
     public static final class drawable {
         public static final int art_clear=0x7f020000;
@@ -43,30 +47,31 @@ public final class R {
         public static final int ic_rain=0x7f02000f;
         public static final int ic_snow=0x7f020010;
         public static final int ic_storm=0x7f020011;
+        public static final int touch_selector=0x7f020012;
     }
     public static final class id {
-        public static final int FrameLayout1=0x7f0b000b;
-        public static final int action_map=0x7f0b0015;
-        public static final int action_refresh=0x7f0b0014;
-        public static final int action_settings=0x7f0b0012;
-        public static final int action_share=0x7f0b0013;
-        public static final int detail_date_textview=0x7f0b0003;
-        public static final int detail_day_textview=0x7f0b0002;
-        public static final int detail_forcast_textview=0x7f0b0007;
-        public static final int detail_high_textview=0x7f0b0004;
-        public static final int detail_humidity_textview=0x7f0b0008;
-        public static final int detail_icon=0x7f0b0006;
-        public static final int detail_low_textview=0x7f0b0005;
-        public static final int detail_pressure_textview=0x7f0b000a;
-        public static final int detail_wind_textview=0x7f0b0009;
-        public static final int fragment_forecast=0x7f0b0001;
-        public static final int listView_forecast=0x7f0b000c;
-        public static final int list_item_date_textview=0x7f0b000e;
-        public static final int list_item_forcast_textview=0x7f0b000f;
-        public static final int list_item_high_textview=0x7f0b0010;
-        public static final int list_item_icon=0x7f0b000d;
-        public static final int list_item_low_textview=0x7f0b0011;
-        public static final int weather_detail_container=0x7f0b0000;
+        public static final int FrameLayout1=0x7f0c000b;
+        public static final int action_map=0x7f0c0015;
+        public static final int action_refresh=0x7f0c0014;
+        public static final int action_settings=0x7f0c0012;
+        public static final int action_share=0x7f0c0013;
+        public static final int detail_date_textview=0x7f0c0003;
+        public static final int detail_day_textview=0x7f0c0002;
+        public static final int detail_forcast_textview=0x7f0c0007;
+        public static final int detail_high_textview=0x7f0c0004;
+        public static final int detail_humidity_textview=0x7f0c0008;
+        public static final int detail_icon=0x7f0c0006;
+        public static final int detail_low_textview=0x7f0c0005;
+        public static final int detail_pressure_textview=0x7f0c000a;
+        public static final int detail_wind_textview=0x7f0c0009;
+        public static final int fragment_forecast=0x7f0c0001;
+        public static final int listView_forecast=0x7f0c000c;
+        public static final int list_item_date_textview=0x7f0c000e;
+        public static final int list_item_forcast_textview=0x7f0c000f;
+        public static final int list_item_high_textview=0x7f0c0010;
+        public static final int list_item_icon=0x7f0c000d;
+        public static final int list_item_low_textview=0x7f0c0011;
+        public static final int weather_detail_container=0x7f0c0000;
     }
     public static final class layout {
         public static final int activity_details=0x7f040000;
@@ -77,69 +82,69 @@ public final class R {
         public static final int list_item_forecast_today=0x7f040005;
     }
     public static final class menu {
-        public static final int details=0x7f0a0000;
-        public static final int forecastfagment=0x7f0a0001;
-        public static final int main=0x7f0a0002;
+        public static final int details=0x7f0b0000;
+        public static final int forecastfagment=0x7f0b0001;
+        public static final int main=0x7f0b0002;
     }
     public static final class mipmap {
         public static final int ic_launcher=0x7f030000;
     }
     public static final class string {
-        public static final int action_map=0x7f07000b;
-        public static final int action_refresh=0x7f070003;
-        public static final int action_settings=0x7f070002;
-        public static final int action_share=0x7f07000c;
-        public static final int app_name=0x7f070000;
-        public static final int hello_world=0x7f070001;
-        public static final int pref_default_display_name=0x7f070014;
-        public static final int pref_description_social_recommendations=0x7f070012;
+        public static final int action_map=0x7f08000b;
+        public static final int action_refresh=0x7f080003;
+        public static final int action_settings=0x7f080002;
+        public static final int action_share=0x7f08000c;
+        public static final int app_name=0x7f080000;
+        public static final int hello_world=0x7f080001;
+        public static final int pref_default_display_name=0x7f080014;
+        public static final int pref_description_social_recommendations=0x7f080012;
         /**  Example settings for Data & Sync 
          */
-        public static final int pref_header_data_sync=0x7f070016;
+        public static final int pref_header_data_sync=0x7f080016;
         /**  Strings related to Settings 
  Example General settings 
          */
-        public static final int pref_header_general=0x7f070010;
+        public static final int pref_header_general=0x7f080010;
         /**  Example settings for Notifications 
          */
-        public static final int pref_header_notifications=0x7f070019;
+        public static final int pref_header_notifications=0x7f080019;
         /**  Default city name for location preference [CHAR LIMIT=NONE] 
          */
-        public static final int pref_location_default=0x7f070007;
+        public static final int pref_location_default=0x7f080007;
         /**  key name for storring location in shared prefernces file 
          */
-        public static final int pref_location_key=0x7f070006;
+        public static final int pref_location_key=0x7f080006;
         /**  Label for location preference 
          */
-        public static final int pref_location_label=0x7f070005;
-        public static final int pref_ringtone_silent=0x7f07001c;
-        public static final int pref_temp_unit_label=0x7f070015;
+        public static final int pref_location_label=0x7f080005;
+        public static final int pref_ringtone_silent=0x7f08001c;
+        public static final int pref_temp_unit_label=0x7f080015;
         /**  key name for storring temperature in shared prefernces file 
          */
-        public static final int pref_temperature_key=0x7f070008;
-        public static final int pref_title_display_name=0x7f070013;
-        public static final int pref_title_new_message_notifications=0x7f07001a;
-        public static final int pref_title_ringtone=0x7f07001b;
-        public static final int pref_title_social_recommendations=0x7f070011;
-        public static final int pref_title_sync_frequency=0x7f070017;
-        public static final int pref_title_system_sync_settings=0x7f070018;
-        public static final int pref_title_vibrate=0x7f07001d;
+        public static final int pref_temperature_key=0x7f080008;
+        public static final int pref_title_display_name=0x7f080013;
+        public static final int pref_title_new_message_notifications=0x7f08001a;
+        public static final int pref_title_ringtone=0x7f08001b;
+        public static final int pref_title_social_recommendations=0x7f080011;
+        public static final int pref_title_sync_frequency=0x7f080017;
+        public static final int pref_title_system_sync_settings=0x7f080018;
+        public static final int pref_title_vibrate=0x7f08001d;
         /**  Imperial Unit type value for storring temperature 
          */
-        public static final int pref_unit_imperial=0x7f07000a;
+        public static final int pref_unit_imperial=0x7f08000a;
         /**  default value for storring temperature which is metric 
          */
-        public static final int pref_unit_metric=0x7f070009;
+        public static final int pref_unit_metric=0x7f080009;
         /**  Title for the second Actitvity 
          */
-        public static final int title_activity_details=0x7f070004;
-        public static final int title_activity_settings=0x7f07000f;
+        public static final int title_activity_details=0x7f080004;
+        public static final int title_activity_settings=0x7f08000f;
         /**  Language-specific constants 
          */
-        public static final int today=0x7f07000d;
+        public static final int today=0x7f08000d;
         /**  For labelling tomorrow's forecast [CHAR LIMIT=15] 
          */
-        public static final int tomorrow=0x7f07000e;
+        public static final int tomorrow=0x7f08000e;
     }
     public static final class style {
         /** 
@@ -163,11 +168,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f090000;
+        public static final int AppBaseTheme=0x7f0a0000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f090001;
+        public static final int AppTheme=0x7f0a0001;
     }
     public static final class xml {
         public static final int pref_data_sync=0x7f050000;
